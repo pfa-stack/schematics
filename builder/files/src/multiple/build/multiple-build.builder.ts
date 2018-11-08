01fcde6488a2cc0a36fdc2e2624e512a9442d367
Emerson Jair <emerson.jair@forrestbin.com>
2018-11-08 07:37:20 -0200

    feat(builder): remove name property from single builder
    also remove the host(from both single and multiple builders) because it was not really useful right
    now
    
    close #24

--- builder/files/src/multiple/build/multiple-build.builder.ts
+++ builder/files/src/multiple/build/multiple-build.builder.ts
@@ -5,8 +5,7 @@ import {
   BuilderContext
 } from '@angular-devkit/architect';
 import { Observable } from 'rxjs';
-import { resolve, tags, virtualFs } from '@angular-devkit/core';
-import { Stats } from 'fs';
+import { resolve, tags } from '@angular-devkit/core';
 
 export interface MultipleBuilderBuildOptions {
   name: string;
@@ -22,9 +21,6 @@ export default class MultipleBuilderBuild
     const options = builderConfig.options;
     const root = this.context.workspace.root;
     const projectRoot = resolve(root, builderConfig.root);
-    const host = new virtualFs.AliasHost(this.context.host as virtualFs.Host<
-      Stats
-    >);
 
     return new Observable(_ => {
       this.context.logger.info(tags.oneLine`
@@ -33,7 +29,6 @@ export default class MultipleBuilderBuild
         Project information:
         * Workspace Root: ${root}
         * Project root: ${projectRoot}
-        * Host: ${host}
       `);
     });
   }
--- builder/files/src/single/single.builder.ts
+++ builder/files/src/single/single.builder.ts
@@ -5,34 +5,22 @@ import {
   BuilderContext
 } from '@angular-devkit/architect';
 import { Observable } from 'rxjs';
-import { resolve, tags, virtualFs } from '@angular-devkit/core';
-import { Stats } from 'fs';
+import { resolve, tags } from '@angular-devkit/core';
 
-export interface SingleBuilderOptions {
-  name: string;
-}
-
-export default class SingleBuilder implements Builder<SingleBuilderOptions> {
+export default class SingleBuilder implements Builder<{}> {
   constructor(public context: BuilderContext) {}
 
-  public run(
-    builderConfig: BuilderConfiguration<SingleBuilderOptions>
-  ): Observable<BuildEvent> {
-    const options = builderConfig.options;
+  public run(builderConfig: BuilderConfiguration<{}>): Observable<BuildEvent> {
     const root = this.context.workspace.root;
     const projectRoot = resolve(root, builderConfig.root);
-    const host = new virtualFs.AliasHost(this.context.host as virtualFs.Host<
-      Stats
-    >);
 
     return new Observable(_ => {
       this.context.logger.info(tags.oneLine`
         **
-        Single builder example. You chose the name ${options.name}.
+        Single builder example.
         Project information:
         * Workspace Root: ${root}
         * Project root: ${projectRoot}
-        * Host: ${host}
       `);
     });
   }
